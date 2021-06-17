5078cb527417f1a195653024944b5d798db28ae8
Mikael Araya <mikaelaraya12@gmail.com>
2021-06-17 18:20:50 +0300

    Update sausage price calculator

--- plugins/sausage.js
+++ plugins/sausage.js
@@ -5,7 +5,7 @@ import {
   import fetch from "isomorphic-unfetch";
   
   const PRODUCT_TAG_SAUSAGE = "sausage";
-  const SAUSAGE_THRESHOLD_CELSIUS = 40;
+  const SAUSAGE_THRESHOLD_CELSIUS = 20;
   
   class WeatherDependentBarbequeSausagePricing extends ProductPricingAdapter {
     static key = "shop.unchained.wd-bbq-sausage-pricing";
@@ -28,19 +28,24 @@ import {
       const { currency, quantity } = this.context;
       try {
         const response = await fetch(
-          "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.temp%20from%20weather.forecast%20where%20woeid%20%3D%20784794&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+          "https://community-open-weather-map.p.rapidapi.com/weather?q=zurich,ch&units=metric", 
+          {
+            headers: {
+              "x-rapidapi-key": "2a849e288dmsh59370f28a9102f6p1c881cjsn28010ce8ff58",
+              "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+              "useQueryString": true
+            }
+          }
         );
         if (response.status === 200) {
-          const { query } = await response.json();
-          const { results, count } = query;
-          if (count > 0) {
-            const { temp: tempFahrenheit } = results.channel.item.condition;
-            const tempCelsius = (parseFloat(tempFahrenheit) - 32) / 1.8;
-            if (tempCelsius > SAUSAGE_THRESHOLD_CELSIUS) {
+          const { main } = await response.json();
+          const { temp} = main;
+          if (temp) {
+            if (temp > SAUSAGE_THRESHOLD_CELSIUS) {
               console.log("🌭 -> High season, sausage pricy!!"); // eslint-disable-line
               this.result.addItem({
                 currency,
-                amount: +100 * quantity,
+                amount: 100 * quantity,
                 isTaxable: true,
                 isNetPrice: true,
                 meta: { adapter: this.constructor.key }
@@ -49,7 +54,7 @@ import {
           }
         }
       } catch (e) {
-        console.error("🌭 -> Failed while trying to price weather dependent"); // eslint-disable-line
+        console.error(`🌭 -> Failed while trying to price weather dependent ${e.message}`); // eslint-disable-line
       }
   
       return super.calculate();
